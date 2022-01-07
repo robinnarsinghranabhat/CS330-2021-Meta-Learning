15ffda113313e0a601a4ad6118510d7768a874e6
unknown <robinnarsingha123@gmail.com>
2022-01-07 22:01:20 +0545

    linted code

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,42 +1,39 @@
 import argparse
 import os
 from pathlib import Path
-from typing_extensions import runtime
+
 import torch
-
 import torch.nn.functional as F
-
-from torch import nn
-from load_data import DataGenerator
-from dnc import DNC
 from google_drive_downloader import GoogleDriveDownloader as gdd
+from torch import nn
 from torch.utils.tensorboard import SummaryWriter
+from typing_extensions import runtime
+
+from dnc import DNC
+from load_data import DataGenerator
 
 
 class MANN(nn.Module):
-
     def __init__(self, num_classes, samples_per_class, model_size=128, input_size=784):
         super(MANN, self).__init__()
-        
+
         def initialize_weights(model):
             nn.init.xavier_uniform_(model.weight_ih_l0)
             nn.init.zeros_(model.bias_hh_l0)
             nn.init.zeros_(model.bias_ih_l0)
-    
+
         self.num_classes = num_classes
         self.samples_per_class = samples_per_class
         self.input_size = input_size
-        self.layer1 = torch.nn.LSTM(num_classes + input_size, 
-                                    model_size, 
-                                    batch_first=True).float()
-        self.layer2 = torch.nn.LSTM(model_size,
-                                    num_classes,
-                                    batch_first=True).float()
+        self.layer1 = torch.nn.LSTM(
+            num_classes + input_size, model_size, batch_first=True
+        ).float()
+        self.layer2 = torch.nn.LSTM(model_size, num_classes, batch_first=True).float()
         initialize_weights(self.layer1)
         initialize_weights(self.layer2)
 
         self.loss_func = nn.CrossEntropyLoss()
-        
+
         # self.dnc = DNC(
         #                input_size=num_classes + input_size,
         #                output_size=num_classes,
@@ -57,11 +54,11 @@ class MANN(nn.Module):
         Args:
             input_images: tensor
                 A tensor of shape [B, K+1, N, 784] of flattened images
-            
+
             labels: tensor:
                 A tensor of shape [B, K+1, N, N] of ground truth labels
         Returns:
-            
+
             out: tensor
             A tensor of shape [B, K+1, N, N] of class predictions
         """
@@ -71,19 +68,22 @@ class MANN(nn.Module):
 
         batch_size, k_plus_1, num_classes, inp_dim = input_images.shape
 
-
         input_labels_cop = torch.clone(input_labels)
-        input_labels_cop[:,-1,:,:] = 0
-        combined_inp_and_label = torch.cat( (input_images,input_labels_cop ), dim=3  )
-        combined_inp_and_label = torch.reshape( combined_inp_and_label, ( batch_size, k_plus_1 * num_classes , num_classes+inp_dim  ))
-        
-        lstm_output_1, (h_1, c_1)  = self.layer1(combined_inp_and_label)
-        lstm_output_2, (h_2, c_2) = self.layer2( lstm_output_1 )
+        input_labels_cop[:, -1, :, :] = 0
+        combined_inp_and_label = torch.cat((input_images, input_labels_cop), dim=3)
+        combined_inp_and_label = torch.reshape(
+            combined_inp_and_label,
+            (batch_size, k_plus_1 * num_classes, num_classes + inp_dim),
+        )
 
-        lstm_output_2 = torch.reshape(lstm_output_2, ( batch_size, k_plus_1, num_classes, num_classes  ) )
-        
+        lstm_output_1, (h_1, c_1) = self.layer1(combined_inp_and_label)
+        lstm_output_2, (h_2, c_2) = self.layer2(lstm_output_1)
+
+        lstm_output_2 = torch.reshape(
+            lstm_output_2, (batch_size, k_plus_1, num_classes, num_classes)
+        )
+
         return lstm_output_2
-
 
     def loss_function(self, preds, labels):
         """
@@ -91,10 +91,10 @@ class MANN(nn.Module):
         Args:
             preds: tensor
                 A tensor of shape [B, K+1, N, N] of network outputs
-            
+
             labels: tensor
                 A tensor of shape [B, K+1, N, N] of class labels
-                
+
         Returns:
             scalar loss
         """
@@ -102,28 +102,27 @@ class MANN(nn.Module):
         #### YOUR CODE GOES HERE ####
         #############################
         # breakpoint()
-        preds = preds[:, -1 , :, :]
-        preds = preds.permute((0,2,1))
+        preds = preds[:, -1, :, :]
+        preds = preds.permute((0, 2, 1))
         # preds = preds.contiguous().view(-1,preds.size(2))
         # convert into class indexes for torch to understand
-        labels = labels[:, -1 , :, :]
-        _, labels  = labels.max(dim=2)
+        labels = labels[:, -1, :, :]
+        _, labels = labels.max(dim=2)
 
         # breakpoint()
 
-        loss = self.loss_func(preds, labels) 
+        loss = self.loss_func(preds, labels)
         return loss
 
-    
-        # SOLUTION:        
-        
-        print('hi')
+        # SOLUTION:
+
+        print("hi")
 
 
 def train_step(images, labels, model, optim):
     predictions = model(images, labels)
     loss = model.loss_function(predictions, labels)
-    
+
     optim.zero_grad()
     loss.backward()
     optim.step()
@@ -137,88 +136,89 @@ def model_eval(images, labels, model):
 
 
 def main(config):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print('USING DEVICE : ', device)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("USING DEVICE : ", device)
 
     run_name = f"K_{config.num_samples}_N_{config.num_classes}_B_{config.meta_batch_size}_H_{config.model_size}"
 
     # Save Artifacts : models, [ Todo : plots, summary ]
-    trained_model_dir = Path(f'./trained_models/{run_name}/')
-    if not os.path.exists(trained_model_dir): 
+    trained_model_dir = Path(f"./trained_models/{run_name}/")
+    if not os.path.exists(trained_model_dir):
         os.makedirs(trained_model_dir)
 
     # Save Runs : Logs, [Todo : hyperparams, ..]
-    run_dir = Path( f'./{config.logdir}' , f'{config.model_type}' , f'{run_name}' )
+    run_dir = Path(f"./{config.logdir}", f"{config.model_type}", f"{run_name}")
     writer = SummaryWriter(run_dir)
 
     # Download Omniglot Dataset
-    if not os.path.isdir('./omniglot_resized'):
-        gdd.download_file_from_google_drive(file_id='1iaSFXIYC3AB8q9K_M-oVMa4pmB7yKMtI',
-                                            dest_path='./omniglot_resized.zip',
-                                            unzip=True)
-    assert os.path.isdir('./omniglot_resized')
+    if not os.path.isdir("./omniglot_resized"):
+        gdd.download_file_from_google_drive(
+            file_id="1iaSFXIYC3AB8q9K_M-oVMa4pmB7yKMtI",
+            dest_path="./omniglot_resized.zip",
+            unzip=True,
+        )
+    assert os.path.isdir("./omniglot_resized")
 
     # Create Data Generator
-    data_generator = DataGenerator(config.num_classes, 
-                                   config.num_samples, 
-                                   device=device)
+    data_generator = DataGenerator(
+        config.num_classes, config.num_samples, device=device
+    )
 
     # Create model and optimizer
-    model = MANN(config.num_classes, config.num_samples, 
-                 model_size=config.model_size)
+    model = MANN(config.num_classes, config.num_samples, model_size=config.model_size)
     model.to(device)
     model.float()
-    
-    optim = torch.optim.Adam(model.parameters(), lr = 1e-3)
-    
-    prev_loss = 10000 ## some high loss
+
+    optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    prev_loss = 10000  ## some high loss
 
     for step in range(config.training_steps):
-        images, labels = data_generator.sample_batch('train', config.meta_batch_size)
+        images, labels = data_generator.sample_batch("train", config.meta_batch_size)
         _, train_loss = train_step(images, labels, model, optim)
 
-        
         if (step + 1) % config.log_every == 0:
-            images, labels = data_generator.sample_batch('test', 
-                                                         config.meta_batch_size)
+            images, labels = data_generator.sample_batch("test", config.meta_batch_size)
             pred, test_loss = model_eval(images, labels, model)
-            pred = torch.reshape(pred, [-1, 
-                                        config.num_samples + 1, 
-                                        config.num_classes, 
-                                        config.num_classes])
+            pred = torch.reshape(
+                pred,
+                [-1, config.num_samples + 1, config.num_classes, config.num_classes],
+            )
             pred = torch.argmax(pred[:, -1, :, :], axis=2)
             labels = torch.argmax(labels[:, -1, :, :], axis=2)
 
             # Log to Tensorboard
-            writer.add_scalar('Train Loss', train_loss, step)
-            writer.add_scalar('Test Loss', test_loss, step)
-            writer.add_scalar('Meta-Test Accuracy', 
-                              pred.eq(labels).double().mean().item(),
-                              step)
-            
+            writer.add_scalar("Train Loss", train_loss, step)
+            writer.add_scalar("Test Loss", test_loss, step)
+            writer.add_scalar(
+                "Meta-Test Accuracy", pred.eq(labels).double().mean().item(), step
+            )
+
             # Help Prints and Model Saving
             test_loss = test_loss.cpu().numpy()
             train_loss = train_loss.cpu().numpy()
 
-            print('AT STEP : ', step)
-            print(' Train Loss : ', train_loss, step )
-            print(' Test Loss : ', test_loss, step )
+            print("AT STEP : ", step)
+            print(" Train Loss : ", train_loss, step)
+            print(" Test Loss : ", test_loss, step)
 
             # Saving the Model
             if test_loss < prev_loss:
-                prev_loss= test_loss
-                torch.save(model.state_dict(), trained_model_dir / f'model_at_step_{step}' )
-                print(f'Model Updated at : step {step}')   
+                prev_loss = test_loss
+                torch.save(
+                    model.state_dict(), trained_model_dir / f"model_at_step_{step}"
+                )
+                print(f"Model Updated at : step {step}")
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_type', type=str, default='LSTM_only')
-    parser.add_argument('--num_classes', type=int, default=5)
-    parser.add_argument('--num_samples', type=int, default=10)
-    parser.add_argument('--meta_batch_size', type=int, default=256)
-    parser.add_argument('--logdir', type=str, 
-                        default='runs')
-    parser.add_argument('--training_steps', type=int, default=10000)
-    parser.add_argument('--log_every', type=int, default=1)
-    parser.add_argument('--model_size', type=int, default=128)
+    parser.add_argument("--model_type", type=str, default="LSTM_only")
+    parser.add_argument("--num_classes", type=int, default=5)
+    parser.add_argument("--num_samples", type=int, default=10)
+    parser.add_argument("--meta_batch_size", type=int, default=256)
+    parser.add_argument("--logdir", type=str, default="runs")
+    parser.add_argument("--training_steps", type=int, default=10000)
+    parser.add_argument("--log_every", type=int, default=1)
+    parser.add_argument("--model_size", type=int, default=128)
     main(parser.parse_args())
--- hw1/load_data.py
+++ hw1/load_data.py
@@ -1,24 +1,25 @@
-import numpy as np
 import os
 import random
 
-from torch._C import device
+import numpy as np
 import torch
+from torch._C import device
 
 
 def one_hot_encode(x, n_classes):
     """
-    One hot encode a list of sample labels. Return a one-hot encoded vector for each label.
+    One hot encode a list of sample labels. 
+    Return a one-hot encoded vector for each label.
     : x: List of sample Labels
     : return: Numpy array of one-hot encoded labels
-     """
+    """
     return np.eye(n_classes)
 
 
 def get_images(paths, labels, nb_samples=None, shuffle=True):
     """
-    Takes a set of character folders and labels and returns paths to image files
-    paired with labels.
+    Takes a set of character folders and labels and 
+    returns paths to image files paired with labels.
     Args:
         paths: A list of character folders
         labels: List or numpy array of same length as paths
@@ -30,9 +31,11 @@ def get_images(paths, labels, nb_samples=None, shuffle=True):
         sampler = lambda x: random.sample(x, nb_samples)
     else:
         sampler = lambda x: x
-    images_labels = [(i, os.path.join(path, image))
-                     for i, path in zip(labels, paths)
-                     for image in sampler(os.listdir(path))]
+    images_labels = [
+        (i, os.path.join(path, image))
+        for i, path in zip(labels, paths)
+        for image in sampler(os.listdir(path))
+    ]
     if shuffle:
         random.shuffle(images_labels)
     return images_labels
@@ -48,6 +51,7 @@ def image_file_to_array(filename, dim_input):
         1 channel image
     """
     import imageio
+
     image = imageio.imread(filename)
     image = image.reshape([dim_input])
     image = image.astype(np.float32) / 255.0
@@ -61,44 +65,45 @@ class DataGenerator(object):
     A "class" is considered a class of omniglot digits.
     """
 
-    def __init__(self, num_classes, num_samples_per_class, config={}, device = 'cpu'):
+    def __init__(self, num_classes, num_samples_per_class, config={}, device="cpu"):
         """
         Args:
             num_classes: int
                 Number of classes for classification (N-way)
-            
+
             num_samples_per_class: int
                 Number of samples per class in the support set (K-shot).
                 Will generate additional sample for the querry set.
-                
-            device: cuda.device: 
+
+            device: cuda.device:
                 Device to allocate tensors to.
         """
         self.num_samples_per_class = num_samples_per_class
         self.num_classes = num_classes
-        
 
-        data_folder = config.get('data_folder', './omniglot_resized')
-        self.img_size = config.get('img_size', (28, 28))
+        data_folder = config.get("data_folder", "./omniglot_resized")
+        self.img_size = config.get("img_size", (28, 28))
 
         self.dim_input = np.prod(self.img_size)
         self.dim_output = self.num_classes
 
-        character_folders = [os.path.join(data_folder, family, character)
-                             for family in os.listdir(data_folder)
-                             if os.path.isdir(os.path.join(data_folder, family))
-                             for character in os.listdir(os.path.join(data_folder, family))
-                             if os.path.isdir(os.path.join(data_folder, family, character))]
-        
+        character_folders = [
+            os.path.join(data_folder, family, character)
+            for family in os.listdir(data_folder)
+            if os.path.isdir(os.path.join(data_folder, family))
+            for character in os.listdir(os.path.join(data_folder, family))
+            if os.path.isdir(os.path.join(data_folder, family, character))
+        ]
+
         random.seed(1)
         random.shuffle(character_folders)
         num_val = 100
         num_train = 1100
-        self.metatrain_character_folders = character_folders[: num_train]
+        self.metatrain_character_folders = character_folders[:num_train]
         self.metaval_character_folders = character_folders[
-            num_train:num_train + num_val]
-        self.metatest_character_folders = character_folders[
-            num_train + num_val:]
+            num_train : num_train + num_val
+        ]
+        self.metatest_character_folders = character_folders[num_train + num_val :]
         self.device = device
 
     def sample_batch(self, batch_type, batch_size):
@@ -107,19 +112,19 @@ class DataGenerator(object):
         Args:
             batch_type: str
                 train/val/test set to sample from
-                
+
             batch_size: int:
                 Size of batch of tasks to sample
-                
+
         Returns:
             images: tensor
                 A tensor of images of size [B, K+1, N, 784]
-                where B is batch size, K is number of samples per class, 
+                where B is batch size, K is number of samples per class,
                 N is number of classes
-                
+
             labels: tensor
-                A tensor of images of size [B, K+1, N, N] 
-                where B is batch size, K is number of samples per class, 
+                A tensor of images of size [B, K+1, N, N]
+                where B is batch size, K is number of samples per class,
                 N is number of classes
         """
         if batch_type == "train":
@@ -139,41 +144,54 @@ class DataGenerator(object):
 
         for b in range(batch_size):
             # Sample N different character Folders
-            sampled_character_classes = random.sample( folders ,  self.num_classes )
+            sampled_character_classes = random.sample(folders, self.num_classes)
             # Sample K images from each of these folders
-            all_images = []            
+            all_images = []
 
             for ind, class_folder in enumerate(sampled_character_classes):
                 # +1 is done, because, 1 example it in the query Set
-                sampled_images_per_character_path = random.sample( os.listdir(class_folder) , self.num_samples_per_class + 1 )
-                sampled_images_per_character_path = [ os.path.join(class_folder, i) for i in sampled_images_per_character_path ]
-                
-                sampled_images_per_character = [ image_file_to_array(i, self.dim_input) for i in sampled_images_per_character_path ]
+                sampled_images_per_character_path = random.sample(
+                    os.listdir(class_folder), self.num_samples_per_class + 1
+                )
+                sampled_images_per_character_path = [
+                    os.path.join(class_folder, i)
+                    for i in sampled_images_per_character_path
+                ]
+
+                sampled_images_per_character = [
+                    image_file_to_array(i, self.dim_input)
+                    for i in sampled_images_per_character_path
+                ]
                 all_images.append(sampled_images_per_character)
 
             # Append the inputs
-            sampled_input.append( all_images )
+            sampled_input.append(all_images)
 
             # Restructure the labels
             class_labels = np.eye(self.num_classes)
-            class_labels = np.tile( class_labels, (1,self.num_samples_per_class + 1))
-            class_labels = np.split(class_labels, self.num_samples_per_class + 1 , axis=1)
+            class_labels = np.tile(class_labels, (1, self.num_samples_per_class + 1))
+            class_labels = np.split(
+                class_labels, self.num_samples_per_class + 1, axis=1
+            )
             sampled_labels.append(class_labels)
 
         # plt.imshow( np.reshape(sampled_input[0 , 0, 0, :], (28,28)  ) ); plt.show()
-        # sampled_input_r = np.reshape(sampled_input, (2, 5, 784))    
-        sampled_input = np.array(sampled_input)    
-        sampled_input =  np.transpose( sampled_input, (  0,2,1,3 ) )
-        sampled_labels = np.array( sampled_labels )
+        # sampled_input_r = np.reshape(sampled_input, (2, 5, 784))
+        sampled_input = np.array(sampled_input)
+        sampled_input = np.transpose(sampled_input, (0, 2, 1, 3))
+        sampled_labels = np.array(sampled_labels)
 
-        # shuffle the set inefficiently !! 
+        # shuffle the set inefficiently !!
         for batch_ind in range(batch_size):
             shuffled_index = np.random.permutation(self.num_classes)
             # index the final kth set of classes & shuffle them
-            train_query_input = sampled_input[ batch_ind , -1, :, : ]
-            sampled_input[batch_ind, -1, :, : ] = train_query_input[ shuffled_index  ]
+            train_query_input = sampled_input[batch_ind, -1, :, :]
+            sampled_input[batch_ind, -1, :, :] = train_query_input[shuffled_index]
             # repeat for labels
-            train_query_label = sampled_labels[ batch_ind , -1, :, : ]
-            sampled_labels[batch_ind, -1, :, : ] = train_query_label[ shuffled_index ]
+            train_query_label = sampled_labels[batch_ind, -1, :, :]
+            sampled_labels[batch_ind, -1, :, :] = train_query_label[shuffled_index]
 
-        return torch.tensor(sampled_input, dtype=torch.float).to(self.device), torch.tensor(sampled_labels, dtype=torch.float).to(self.device)
+        return (
+            torch.tensor(sampled_input, dtype=torch.float).to(self.device),
+            torch.tensor(sampled_labels, dtype=torch.float).to(self.device),
+        )
